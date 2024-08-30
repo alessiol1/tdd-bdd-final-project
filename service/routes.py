871c4aa4d4854761bf46ea93130a5649338564ce
Alessio <alessio@leins.de>
2024-08-30 05:08:31 -0400

    Updated

--- service/routes.py
+++ service/routes.py
@@ -23,6 +23,7 @@ from flask import url_for  # noqa: F401 pylint: disable=unused-import
 from service.models import Product
 from service.common import status  # HTTP Status Codes
 from . import app
+from service.models import Product, Category
 
 
 ######################################################################
@@ -104,18 +105,33 @@ def create_products():
 ######################################################################
 # LIST PRODUCTS
 ######################################################################
-
 @app.route("/products", methods=["GET"])
 def list_products():
     """Returns a list of Products"""
     app.logger.info("Request to list Products...")
-
-    products = Product.all()
-
+    products = []
+    name = request.args.get("name")
+    category = request.args.get("category")
+    available = request.args.get("available")
+    if name:
+        app.logger.info("Find by name: %s", name)
+        products = Product.find_by_name(name)
+    elif category:
+        app.logger.info("Find by category: %s", category)
+        # create enum from string
+        category_value = getattr(Category, category.upper())
+        products = Product.find_by_category(category_value)
+    elif available:
+        app.logger.info("Find by available: %s", available)
+        # create bool from string
+        available_value = available.lower() in ["true", "yes", "1"]
+        products = Product.find_by_availability(available_value)
+    else:
+        app.logger.info("Find all")
+        products = Product.all()
     results = [product.serialize() for product in products]
     app.logger.info("[%s] Products returned", len(results))
     return results, status.HTTP_200_OK
-
 ######################################################################
 # R E A D   A   P R O D U C T
 ######################################################################
@@ -139,14 +155,6 @@ def get_products(product_id):
 ######################################################################
 # U P D A T E   A   P R O D U C T
 ######################################################################
-
-#
-# PLACE YOUR CODE TO UPDATE A PRODUCT HERE
-#
-######################################################################
-# UPDATE AN EXISTING PRODUCT
-######################################################################
-
 @app.route("/products/<int:product_id>", methods=["PUT"])
 def update_products(product_id):
     """
